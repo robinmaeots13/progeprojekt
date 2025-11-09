66b2d345a922af0fe235850c896322f359b3986b
eduardpavlovv-collab <eduard.pavlovv@gmail.com>
2025-11-09 15:09:54 +0200

    Lisasin ka teised pangad - lhv, swed ja seb

--- kulude_ja_tulude_automaatne_jälgija.py
+++ kulude_ja_tulude_automaatne_jälgija.py
@@ -1,4 +1,4 @@
-pank = input("Sisesta panga nimi: ")
+pank = input("Sisesta panga nimi: ").strip().lower()
 csv_fail = input("Sisesta .csv faili nimi: ")
 
 def kuva_kulud_tulud(pank, csv_fail):
@@ -9,12 +9,15 @@ def kuva_kulud_tulud(pank, csv_fail):
         print(f"Viga: Faili '{csv_fail}' ei leitud.")
         return
 
-    if pank == "Luminor":
+    if pank == "luminor": # Luminor CSV formaadi töötlemine
         kulud = 0.0
         tulud = 0.0
 
         for rida in read_data[1:]:  # Jäta vahele päis
             veerud = rida.strip().split(';')
+
+            if len(veerud) < 9: # Kontrolli, kas veerge on piisavalt. Kui ei, jätka järgmise reaga
+                continue
 
             summa_str = veerud[6].replace('"', '').replace(',', '.').strip() # Teisenda koma punktiks ja eemalda jutumärgid
 
@@ -28,6 +31,78 @@ def kuva_kulud_tulud(pank, csv_fail):
                 kulud += abs(summa)
             else:
                 tulud += summa
+
+        print(f"Panga '{pank}' kulud: {kulud:.2f} EUR")
+        print(f"Panga '{pank}' tulud: {tulud:.2f} EUR")
+
+    if pank == "lhv": # LHV panga CSV formaadi töötlemine
+        kulud = 0.0
+        tulud = 0.0
+
+        for rida in read_data[1:]:  # Jäta vahele päis
+            veerud = rida.strip().split(',')
+
+            if len(veerud) < 9: # Kontrolli, kas veerge on piisavalt. Kui ei, jätka järgmise reaga
+                continue
+
+            summa = float(veerud[8])
+
+            if veerud[7].strip('"') == "C":
+                tulud += summa
+            elif veerud[7].strip('"') == "D":
+                kulud += abs(summa)
+
+        print(f"Panga '{pank}' kulud: {kulud:.2f} EUR")
+        print(f"Panga '{pank}' tulud: {tulud:.2f} EUR")
+
+    if pank == "swedbank" or pank == 'swedpank' or pank == 'swed': # Swedbank panga CSV formaadi töötlemine
+        kulud = 0.0
+        tulud = 0.0
+
+        for rida in read_data[1:-3]:  # Jäta vahele päis
+            veerud = rida.strip().split(';')
+
+            if len(veerud) < 9: # Kontrolli, kas veerge on piisavalt. Kui ei, jätka järgmise reaga
+                continue
+
+            summa_str = veerud[5].replace('"', '').replace(',', '.').strip() # Teisenda koma punktiks ja eemalda jutumärgid
+
+            try:
+                summa = float(summa_str)
+            except ValueError:
+                print(f"Viga rea väärtuse teisendamisel: {veerud[5]}")
+                continue  # jätab vahele rea, kui teisendamine ebaõnnestub
+
+            if veerud[7].strip('"') == "K":
+                tulud += summa
+            elif veerud[7].strip('"') == "D":
+                kulud += abs(summa)
+
+        print(f"Panga '{pank}' kulud: {kulud:.2f} EUR")
+        print(f"Panga '{pank}' tulud: {tulud:.2f} EUR")
+
+    if pank == "seb": # SEB CSV formaadi töötlemine
+        kulud = 0.0
+        tulud = 0.0
+
+        for rida in read_data[1:]:  # Jäta vahele päis
+            veerud = rida.strip().split(';')
+            
+            if len(veerud) < 9: # Kontrolli, kas veerge on piisavalt. Kui ei, jätka järgmise reaga
+                continue
+
+            summa_str = veerud[8].replace('"', '').replace(',', '.').strip() # Teisenda koma punktiks ja eemalda jutumärgid
+
+            try:
+                summa = float(summa_str)
+            except ValueError:
+                print(f"Viga rea väärtuse teisendamisel: {veerud[8]}")
+                continue  # jätab vahele rea, kui teisendamine ebaõnnestub
+
+            if veerud[7].strip('"') == "C":
+                tulud += summa
+            elif veerud[7].strip('"') == "D":
+                kulud += abs(summa)
 
         print(f"Panga '{pank}' kulud: {kulud:.2f} EUR")
         print(f"Panga '{pank}' tulud: {tulud:.2f} EUR")
